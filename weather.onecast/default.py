3c7f77f7c94fa6ea72a2dd7ec0284e234e5c52da
infinic <admlynx@gmail.com>
2018-02-16 00:11:13 +0500

    Base functional of addon

---
+++ weather.onecast/default.py
@@ -0,0 +1,7 @@
+﻿import xbmc
+import xbmcgui
+
+WINDOW = xbmcgui.Window(12600)
+WINDOW.setProperty("Current.Temperature", "-15")
+WINDOW.setProperty("Current.Humidity", "20")
+WINDOW.setProperty("Current.Wind", "120")
